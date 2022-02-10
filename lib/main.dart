c0823b61145bff6c52b51d0ea740e5c9e50325c3
Zubair <zubairbhuian@gmail.com>
2022-02-10 16:39:50 +0600

    flutter10.4

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(MaterialApp(
-    home: Container(
-      color: Colors.purple,
-      child: Text('hi everyone'),
-    ),
+    home: HomePage(),
+    theme: ThemeData(
+      primarySwatch:Colors.purple),
   ));
+}
+
+class HomePage extends StatelessWidget {
+  // const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text("Awesome App"),
+      ),
+    );
+  }
 }
