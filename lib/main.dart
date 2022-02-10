af66cff3bfedca1a6d75097e15eaa8aa90750b06
Zubair <zubairbhuian@gmail.com>
2022-02-10 17:30:54 +0600

    flutter10.7

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,40 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    home:const HomePage(),
     theme: ThemeData(
+      
       primarySwatch:Colors.purple),
   ));
 }
 
 class HomePage extends StatelessWidget {
-  // const HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title:const Text("Awesome App"),
+      ),
+      body:Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.blue[600],
+          width: 100,
+          height: 100,
+          alignment:Alignment.center,
+          
+          child: const Text(
+            "I am a box",
+            style:TextStyle(
+              fontSize: 16,
+              color: Colors.white
+            ),
+            ),
+        ),
       ),
     );
   }
