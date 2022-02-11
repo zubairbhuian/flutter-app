fe45a8b8980b16ff2bcd0a0c3127d49cdfa1f904
Zubair <zubairbhuian@gmail.com>
2022-02-11 07:41:11 +0600

    flutter11.1

--- lib/main.dart
+++ lib/main.dart
@@ -21,10 +21,13 @@ class HomePage extends StatelessWidget {
         child: ListView(
           padding: const EdgeInsets.all(0),
           children:[
-            DrawerHeader(
-              child: Text('Hi, I am a Drawer'),
-              decoration: BoxDecoration(color: Colors.green),
-            ),
+            UserAccountsDrawerHeader(
+              accountName: Text('Zubair'),
+               accountEmail: Text('bhuian@gmail.com'),
+               currentAccountPicture:CircleAvatar(
+                 backgroundImage: NetworkImage("https://scontent.fdac80-1.fna.fbcdn.net/v/t1.6435-9/123042841_945908579151890_6246602248743437967_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeE_H0d-mH39OysN5a0qVKJzONjtsS8E12I42O2xLwTXYuSdhNRxWIyj59WjUrCCHZGS3F2ibpUTzwGM7PXONha2&_nc_ohc=wlDNajBViV4AX9hkHAJ&tn=awbMQwkPTCWm_hVB&_nc_ht=scontent.fdac80-1.fna&oh=00_AT9-DcYPAUolZ_mU1O1dSHiE4PIHNRhjE0rLNo2tOwbNaw&oe=622ADFEB")
+                 )
+              ),
             ListTile(
                 leading: Icon(Icons.person),
                 title: Text('Bhuian'),
