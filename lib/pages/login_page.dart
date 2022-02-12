285d0df3ce1839248d6d9129055e4eccb7f70dc4
Zubair <zubairbhuian@gmail.com>
2022-02-12 21:15:26 +0600

    flutter12.9

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   // const LoginPage({Key? key}) : super(key: key);
   final formHey = GlobalKey<FormState>();
+
   final _userNameController = TextEditingController();
+
   final _passwordController = TextEditingController();
 
   @override
@@ -15,11 +22,42 @@ class LoginPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Form(
           key: formHey,
-          child: Column(
-            children: [TextFormField(
-              controller: _userNameController,
-              keyboardType: TextInputType.emailAddress,
-            )],
+          child: Center(
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children:<Widget> [
+                    TextFormField(
+                    controller: _userNameController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      hintText: "Enter email",
+                      labelText: "Username"
+                    ),
+                  ),
+                    TextFormField(
+                    controller: _passwordController,
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: "Enter password",
+                      labelText: "Password"
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  RaisedButton(
+                    onPressed: (){},
+                    child: Text("Sign In"),
+                    color: Colors.orange,
+                    textColor: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ),
