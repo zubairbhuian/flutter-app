7b8de8ec32943944eacb310ba01652ce64dfcaf4
Zubair <zubairbhuian@gmail.com>
2022-02-13 11:12:17 +0600

    flutter13.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/home.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
+void main () =>runApp(const MyApp());
 
-void main() {
-  runApp(const MyApp());
-}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/home.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -16,51 +17,66 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login page"),
-      ),
-      body: SingleChildScrollView(
-        child: Form(
-          key: formHey,
-          child: Center(
-            child: Card(
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  children:<Widget> [
-                    TextFormField(
-                    controller: _userNameController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      hintText: "Enter email",
-                      labelText: "Username"
+        appBar: AppBar(
+          title: Text("Login page"),
+        ),
+        body: Stack(fit: StackFit.expand, children: [
+          Image.asset(
+            "assets/one.jpg",
+            fit: BoxFit.cover,
+            color: Colors.black.withOpacity(0.7),
+            colorBlendMode: BlendMode.darken,
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: formHey,
+                  child: Center(
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Column(
+                          children: <Widget>[
+                            TextFormField(
+                              controller: _userNameController,
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: const InputDecoration(
+                                  hintText: "Enter email",
+                                  labelText: "Username"),
+                            ),
+                            TextFormField(
+                              controller: _passwordController,
+                              keyboardType: TextInputType.text,
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                  hintText: "Enter password",
+                                  labelText: "Password"),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            RaisedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => HomePage()));
+                              },
+                              child: Text("Sign In"),
+                              color: Colors.orange,
+                              textColor: Colors.white,
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
-                    TextFormField(
-                    controller: _passwordController,
-                    keyboardType: TextInputType.text,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      hintText: "Enter password",
-                      labelText: "Password"
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  RaisedButton(
-                    onPressed: (){},
-                    child: Text("Sign In"),
-                    color: Colors.orange,
-                    textColor: Colors.white,
-                    ),
-                  ],
                 ),
               ),
             ),
           ),
-        ),
-      ),
-    );
+        ]));
   }
 }
